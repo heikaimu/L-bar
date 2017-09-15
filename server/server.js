ae4d3fcb08bf549abb76925aa56aaf5a8763656f
heikaimu <576079353@qq.com>
2017-09-15 13:39:00 +0800

    .

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ server.use(cookieParser());
 
 server.all('*', function(req, res, next) {
 	res.header('Access-Control-Allow-Credentials', 'true');
-    res.header("Access-Control-Allow-Origin", "http://192.168.1.4:8080");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1')
